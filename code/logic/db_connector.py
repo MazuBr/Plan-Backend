daeb164d47271742d744fa10b6c9c9521a4f5ced
Matv <syusyukin@nlmk.com>
2024-07-09 18:46:01 +0400

    feat: add connector

---
+++ code/logic/db_connector.py
@@ -0,0 +1,10 @@
+import psycopg2
+from config import DB_CONF
+
+def create_connector():
+    try:
+        connect = psycopg2.connect(**DB_CONF)
+    except psycopg2.Error as e:
+        return None
+    
+    return connect
