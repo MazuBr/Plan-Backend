69e85ebb211ec0dc8504b5777de93f451d2a72a8
Matv <syusyukin@nlmk.com>
2024-07-09 19:58:52 +0400

    feat: add roles

--- db/init_users.sql
+++ db/init_users.sql
@@ -1,18 +1,23 @@
-CREATE TYPE userroles AS ENUM ('admin', 'demo', 'user');
-
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
-    "role" userroles DEFAULT 'user',
+    "role" smallint,
     email VARCHAR(100) UNIQUE NOT NULL,
     "password" VARCHAR(255) NOT NULL,
     first_name VARCHAR(100),
     last_name VARCHAR(100),
     phone VARCHAR(20),
     "address" TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_at BIGINT DEFAULT EXTRACT(EPOCH FROM CURRENT_TIMESTAMP),
+    updated_at BIGINT DEFAULT EXTRACT(EPOCH FROM CURRENT_TIMESTAMP),
     is_deleted BOOLEAN DEFAULT FALSE,
-    deleted_at TIMESTAMP
+    deleted_at BIGINT
 );
 
+
+CREATE TABLE roles (
+    id SMALLSERIAL PRIMARY KEY,
+    name user_roles UNIQUE NOT NULL
+)
+
+INSERT INTO roles (role_name) VALUES ('admin'), ('user'), ('demo');
