7fb2fb93e2c3bf15abc949f0c609b2b322305dba
Matv <syusyukin@nlmk.com>
2024-07-12 01:46:26 +0400

    feat: remove cors

--- code/main.py
+++ code/main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 from routes.users import user_router
 
 
 app = FastAPI()
+app.add_middleware(CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user_router, prefix='/user', tags=['users'])
 
