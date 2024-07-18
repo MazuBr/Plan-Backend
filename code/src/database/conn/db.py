508af2240995b22e317b4aab0ad9f8345d9fdd37
Matv <syusyukin@nlmk.com>
2024-07-18 22:57:15 +0400

    test

--- code/src/database/conn/db.py
+++ code/src/database/conn/db.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, MetaData
-from src.config import DATABASE_URL_TEST 
-engine = create_engine(DATABASE_URL_TEST)
+from src.config import DATABASE_URL 
+engine = create_engine(DATABASE_URL)
 meta = MetaData()
 
 conn = engine.connect()
