f8ee7c1d657b0eb0b97da27ab5eae66f71f1c0df
Matv <syusyukin@nlmk.com>
2024-07-29 17:51:03 +0400

    feat: add select by user id

--- code/src/graphql/queries/calendar.py
+++ code/src/graphql/queries/calendar.py
@@ -1,13 +1,24 @@
+from fastapi import Request
 import strawberry
+from strawberry.types import info
 
 from src.database.postgres_connection import Database
-from src.graphql.types.calendar import CalendarGetEvents,\
-      CalendarHumanReadable, Repeat, CalendarEventsByDay, Calendar
+from src.graphql.types.calendar import (
+    CalendarGetEvents,
+    CalendarHumanReadable,
+    Repeat,
+    CalendarEventsByDay,
+    Calendar,
+)
+
 
 @strawberry.type
 class CalendarQuery:
     @strawberry.field
-    def calendar(self, input: CalendarGetEvents) -> list[CalendarEventsByDay]:
+    def calendar(
+        self, input: CalendarGetEvents, info: info
+    ) -> list[CalendarEventsByDay]:
+        user_id: int = info.context.get("request").state.user_id
         query = """
         SELECT
             id,
@@ -20,38 +31,50 @@ class CalendarQuery:
             repeat_until
         FROM
             calendar
+        LEFT JOIN calendar_user_association on calendar_id = id
         WHERE
             start_time BETWEEN %(start_time)s AND %(end_time)s
+            and user_id = %(user_id)s
             and is_delete = false
         ORDER BY
             start_time ASC;
         """
         db = Database()
 
-        db_response: list[dict] = db.fetch_all(query=query, params={'start_time': input.start_time, 'end_time': input.end_time})
+        db_response: list[dict] = db.fetch_all(
+            query=query,
+            params={
+                "start_time": input.start_time,
+                "end_time": input.end_time,
+                "user_id": user_id,
+            },
+        )
+
         events_by_day = {}
         for row in db_response:
             event = CalendarHumanReadable(
-                    id=row.get('id'),
-                    title=row.get('title'),
-                    comment=row.get('comment'),
-                    day_event_start=row.get('start_time'),
-                    end_time=row.get('end_time'),
-                    repeat=Repeat(is_repeat=row.get('is_repeat'), repeat_until=row.get('repeat_until'))
-                )
-            event_date = row.get('event_date')
+                id=row.get("id"),
+                title=row.get("title"),
+                comment=row.get("comment"),
+                day_event_start=row.get("start_time"),
+                end_time=row.get("end_time"),
+                repeat=Repeat(
+                    is_repeat=row.get("is_repeat"), repeat_until=row.get("repeat_until")
+                ),
+            )
+            event_date = row.get("event_date")
             if event_date not in events_by_day:
                 events_by_day[event_date] = []
             events_by_day[event_date].append(event)
-            
+
         return [
-            CalendarEventsByDay(day=day, events=events) for day, events in events_by_day.items()
+            CalendarEventsByDay(day=day, events=events)
+            for day, events in events_by_day.items()
         ]
-    
 
     @strawberry.field
     def calendar_epoch(self, input: CalendarGetEvents) -> list[Calendar]:
-        print('input1: ', input)
+        print("input1: ", input)
         query = """
         SELECT
             id,
@@ -71,14 +94,21 @@ class CalendarQuery:
         """
         db = Database()
 
-        db_response: list[dict] = db.fetch_all(query=query, params={'start_time': input.start_time, 'end_time': input.end_time})
-            
-        return [Calendar(
-                id=row.get('id'),
-                title=row.get('title'),
-                comment=row.get('comment'),
-                start_time=row.get('start_time'),
-                end_time=row.get('end_time'),
-                repeat=Repeat(is_repeat=row.get('is_repeat'), repeat_until=row.get('repeat_until'))
-            ) for row in db_response]
-    
+        db_response: list[dict] = db.fetch_all(
+            query=query,
+            params={"start_time": input.start_time, "end_time": input.end_time},
+        )
+
+        return [
+            Calendar(
+                id=row.get("id"),
+                title=row.get("title"),
+                comment=row.get("comment"),
+                start_time=row.get("start_time"),
+                end_time=row.get("end_time"),
+                repeat=Repeat(
+                    is_repeat=row.get("is_repeat"), repeat_until=row.get("repeat_until")
+                ),
+            )
+            for row in db_response
+        ]
--- db/init_calendar.sql
+++ db/init_calendar.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS public.calendar
     end_time bigint,
     is_repeat boolean,
     repeat_until bigint,
-    is_delete bool,
+    is_delete BOOLEAN DEFAULT FALSE,
     deleted_by bigint,
     CONSTRAINT calendar_users_id_fkey FOREIGN KEY (deleted_by)
         REFERENCES public.users (id) MATCH SIMPLE
@@ -17,6 +17,7 @@ CREATE TABLE IF NOT EXISTS public.calendar
 
 CREATE TABLE IF NOT EXISTS public.calendar_user_association
 (
+    id BIGSERIAL PRIMARY KEY,
     calendar_id bigint,
     user_id bigint,
     CONSTRAINT calendar_user_association_calendar_id_fkey FOREIGN KEY (calendar_id)
